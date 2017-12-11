f31be0368a6270e901e225505ba20d010188ff51
David Mamou <david_mamou@live.fr>
2017-12-11 15:39:04 +0200

    added camera action and photo library

--- Furniture/Controller/FurnitureDetailVC.swift
+++ Furniture/Controller/FurnitureDetailVC.swift
@@ -43,6 +43,13 @@ class FurnitureDetailVC: UIViewController {
         let alertController = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alertController.addAction(cancelAction)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { action in print("User chose Camera")
+            })
+        alertController.addAction(cameraAction)
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: {action in print("User chose Photo Library")
+        })
+        alertController.addAction(photoLibraryAction)
         present(alertController, animated: true, completion: nil)
         
         
